7c685c402eb9a94922ee2f246aa67ad546c8257e
Cesar Morales <cesarlenin2@live.com>
2020-03-17 09:50:41 -0700

    added a compute average function

--- loops.js
+++ loops.js
@@ -1,1 +1,11 @@
+'use strict';
 
+function average (num){
+  let sum= 0;
+  num.forEach(element => {
+    sum += element;   
+  });
+  return sum/num.length;
+}
+
+console.log(average([5,6,9,5,2,4]));
