bd0ff84a6659e4f0c870675998a9ae3d25c4db5a
Cesar Morales <cesarlenin2@live.com>
2020-03-17 12:27:27 -0700

    syntax on objects.js

--- objects.js
+++ objects.js
@@ -71,9 +71,9 @@ const sampleObj = {
 
 //---------------------------------keyDeleter--------------------------------------------------------------
 function keyDeleter(obj) {
-	delete obj.foo;
-	delete obj.bar;
-	return obj;
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
 }
 
 /* From here down, you are not expected to 
